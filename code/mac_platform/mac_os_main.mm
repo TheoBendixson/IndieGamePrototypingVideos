cb74069f6b42e0733aff1833289dc7387db04c73
Theodore Bendixson <tedbendixson@me.com>
2021-08-31 20:48:23 -0400

    Day 003

--- code/mac_platform/mac_os_main.mm
+++ code/mac_platform/mac_os_main.mm
@@ -1,10 +1,76 @@
 #import <AppKit/AppKit.h>
 
-// NOTE: (Ted) Code Entry Point
+#if 0
+struct window
+{
+    // the data...
+    float X, Y;
+    float Width, Height;
+    uint_32 BackgroundColor;
+};
+
+class NSWindow {
+
+    // properties not directly accessible.
+
+    // there is a struct in here!
+    struct window
+    {
+        // the data...
+        float X, Y;
+        float Width, Height;
+        uint_32 BackgroundColor;
+    };
+
+    // Initializers
+    
+
+    // Constructors
+    // Destructors
+
+    @property float Width;
+
+    // Getters and Setters (A.K.A. "accessors")
+    - (float) GetWidth {
+        return _Width;
+    }
+
+
+    // Because you don't have direct access to the data!!
+
+    /*
+    real32 X, Y;
+    real32 Width, Height;
+    uint32 BackgroundColor;*/
+}
+
+struct window
+{
+    NSRect WindowRect;
+    styleMask;
+    backingStoreType;
+    deferFlag;
+    NSColor BackgroundColor;
+    NSString Title;
+}
+#endif
+
 int main(int argc, const char * argv[]) 
 {
     NSLog(@"Mooselutions is running!");    
-    return 0;
+
+    NSRect WindowRectangle = NSMakeRect(0.0f, 0.0f, 1024.0f, 1024.f);
+
+    NSWindow *Window = [[NSWindow alloc] initWithContentRect: WindowRectangle 
+                                                   styleMask: (NSWindowStyleMaskTitled |
+                                                               NSWindowStyleMaskClosable)
+                                                     backing: NSBackingStoreBuffered 
+                                                       defer: NO];
+
+    [Window setBackgroundColor: [NSColor redColor]];
+    [Window setTitle: @"Mooselutions"];
+    [Window makeKeyAndOrderFront: nil];
+
+    return NSApplicationMain(argc, argv);
 }
 
-//return UIApplicationMain(argc, argv, nil, appDelegateClassName);
